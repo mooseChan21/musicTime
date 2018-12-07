d26ce884a223b5e73af13dd2c7a9b54454db9645
nierq <nierq@pchilltech.com>
2018-12-07 23:30:54 +0800

    渲染层错误

--- components/like/index.js
+++ components/like/index.js
@@ -4,15 +4,7 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    like:{
-      type: Boolean,
-    },
-    count:{
-      type:Number
-    },
-    readOnly:{
-      type:Boolean
-    }
+ 
   },
 
   /**
@@ -25,32 +17,13 @@ Component({
     // 封装在内部 ，开放出来的
     // 粒度
     // 非常简单的功能   非常复杂的功能
-    yesSrc: 'images/like.png',
-    noSrc: 'images/like@dis.png'
+  
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    onLike:function(event){
-      // 自定义事件
-      if(this.properties.readOnly){
-        return
-      }
-      let like = this.properties.like
-      let count = this.properties.count
 
-      count = like?count-1:count+1
-      this.setData({
-        count:count,
-        like:!like
-      })
-      // 激活
-      let behavior = this.properties.like?'like':'cancel'
-      this.triggerEvent('like',{
-        behavior:behavior
-      },{})
-    }
   }
 })
