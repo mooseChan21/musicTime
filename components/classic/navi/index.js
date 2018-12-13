6e9c9d99407b813fc68711f5e667df74679810a0
mooseChan21 <578002054@qq.com>
2018-12-13 14:42:56 +0800

    'add'

---
+++ components/classic/navi/index.js
@@ -0,0 +1,28 @@
+// components/classic/navi/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {  // 暴露到使用组件的page 作为属性传递
+    // title:'饮食男女',
+    // first:'',
+    // lastest:''
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+      //  disLeftSrc:'images/triangle.dis@left.png',
+      //  disRightSrc:'images/triangle.dis@right.png',
+      //  leftSrc:'images/triangle@left.png',
+      //  rightSrc:'images/triangle@right.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
